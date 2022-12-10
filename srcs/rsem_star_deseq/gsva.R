de484765393f80948995bb3eb24628ac199f3958
jigsaw-snu <sglee0723@snu.ac.kr>
2022-12-11 00:45:22 +0900

    working on single sample GSEA

--- srcs/rsem_star_deseq/gsva.R
+++ srcs/rsem_star_deseq/gsva.R
@@ -21,9 +21,16 @@ mod <- stats::model.matrix(~ factor(leukemia_es$subtype))  # design matrix ; sha
 colnames(mod) <- c("ALL", "MLL.vs.ALL")
 
 fit <- limma::lmFit(leukemia_es, mod)  # fit gene-wise linear models ; in here, fit "pathway-wise" linear models
-fit <- limma::eBayes(fit)
+# above expression is equal to limma::lmFit(exprs(leukemia_es), mod)
+# above expression is also equal to limma::lmFit(leukemia_es@assayData$exprs, mod)
+# exprs(leukemia_es) (same as leukemia_es@assayData$exprs) has actual (pathway x samples) ES values
 
-res <- limma::decideTests(fit, p.value=0.01)
+fit.e <- limma::eBayes(fit)  # compute (empirical Bayes) moderated t-statistics / F-statistics / log-odds
 
-tt <- limma::topTable(fit, coef=2, n=Inf)
+res <- limma::decideTests(fit.e, p.value=0.01)
+
+tt <- limma::topTable(fit.e, coef=2, n=Inf)
+
+DEpwys <- rownames(tt)[tt$adj.P.Val <= 0.01]
+DEpwys_es <- exprs(leukemia_es[DEpwys, ])
 ################################################################################
