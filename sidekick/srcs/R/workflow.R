b24310e4d0248e1e3176800a2742990a62ae24b7
jigsaw-snu <sglee0723@snu.ac.kr>
2023-02-27 21:53:37 +0900

    adding codes for Quality Control

--- sidekick/srcs/R/workflow.R
+++ sidekick/srcs/R/workflow.R
@@ -6,8 +6,11 @@ RDATA <- file.path(RSRC, "rdata")
 FIGURES <- file.path(RSRC, "figures")
 
 
+
+
 # 1. create count_data and meta_data
 library(tidyverse)
+
 
 # prepare count data
 files <- list.files(RAW_DATA)
@@ -69,15 +72,17 @@ gene_length[] <- lapply(gene_length, function(x) round(x))
 tpm[] <- lapply(tpm, function(x) round(x))
 fpkm[] <- lapply(fpkm, function(x) round(x))
 
-
-# gene 
 # remove low count 'genes' (count_value <= 15)
-raw_count <- raw_count[apply(raw_count, MARGIN = 1, function(x) all(x > 10)), ]
+count_data <- raw_count[apply(raw_count, MARGIN = 1, function(x) all(x > 10)), ]
+
+# EDA for read counts
+boxplot(count_data)  # show boxplot for read counts
 
 # find and remove outlier 'genes' using MAD criteria
 
 
-# feature selection (use highly variable genes)
+# feature selection (use highly variable genes) : genes with low variance have no information
+
 
 
 # prepare meta data
@@ -92,10 +97,23 @@ map_dose <- c("TRUE" = "3x", "FALSE" = "1x")
 map_diet <- c("TRUE" = "nash_diet", "FALSE" = "normal_chow")
 map_tumor <- c("TRUE" = "tumor", "FALSE" = "non-tumor")
 
+stage <- c()
+
+for (sample_name in colnames(count_data)) {
+    switch(
+        strsplit(sample_name, '_')[[1]][1],
+        "CCI4" = stage <- c(stage, "Fibrosis"),
+        "HCC" = stage <- c(stage, "HCC"),
+        "NASH" = stage <- c(stage, "NASH"),
+        "NCD" = stage <- c(stage, "Normal")
+    )
+}
+
 meta_data <- data.frame(Treatment = map_treatment[as.character(treatment)],
                         Dose = map_dose[as.character(dose)],
                         Diet = map_diet[as.character(diet)],
-                        Tumor = map_tumor[as.character(tumor)])
+                        Tumor = map_tumor[as.character(tumor)],
+                        Stage = stage)
 
 rownames(meta_data) <- colnames(raw_count)
 
@@ -103,16 +121,44 @@ meta_data$Treatment <- factor(meta_data$Treatment, levels = c("saline", "ccl4"))
 meta_data$Dose <- factor(meta_data$Dose, levels = c("1x", "3x"))
 meta_data$Diet <- factor(meta_data$Diet, levels = c("normal_chow", "nash_diet"))
 meta_data$Tumor <- factor(meta_data$Tumor, levels = c("non-tumor", "tumor"))
+meta_data$Stage <- factor(meta_data$Stage, levels = c("Normal", "NASH", "Fibrosis", "HCC"))
 
 
 
 
 # 2. make DESeq object
 library(DESeq2)
+library(ggrepel)
 
-dds <- DESeq2::DESeqDataSetFromMatrix(countData =)
+
+dds <- DESeq2::DESeqDataSetFromMatrix(countData = count_data,
+                                      colData = meta_data,
+                                      design = ~Stage)
+
+dds <- DESeq2::DESeq(dds)
 
 
 
 
 # 3. EDA
+rlog <- DESeq2::rlog(dds, blind = TRUE)
+rlog_backup <- rlog
+
+rlog <- SummarizedExperiment::assay(rlog)
+
+pca <- stats::prcomp(t(rlog), center = TRUE, scale. = TRUE)
+percent_var <- round(pca$sdev^2 / sum(pca$sdev^2) * 100, digits = 2)
+pca_df <- data.frame(PC1 = pca$x[, 1], PC2 = pca$x[, 2], PC3 = pca$x[, 3])
+pca_df <- merge(pca_df, meta_data, by = "row.names") %>%
+    tibble::column_to_rownames(var = "Row.names")
+
+ggplot2::ggplot(pca_df, aes(x = PC1, y = PC2)) + 
+    ggplot2::geom_point(size = 4, aes(color = Stage, shape = Dose)) +
+    ggplot2::xlab(paste0("PC1 : ", percent_var[1], '%')) +
+    ggplot2::ylab(paste0("PC2 : ", percent_var[2], '%')) +
+    ggplot2::theme_bw()
+    #ggplot2::coord_fixed(ratio = sqrt(percent_var[2] / percent_var[1]))
+    
+    ggrepel::geom_text_repel(aes(label = rownames(pca_df)))
+
+DESeq2::plotPCA(rlog_backup, intgroup = "Stage")
