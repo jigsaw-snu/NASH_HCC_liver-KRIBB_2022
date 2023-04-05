a3de8cb18ff9ee2d97b1b333a13d77d2a9115cf1
jigsaw-snu <sglee0723@snu.ac.kr>
2023-04-05 10:25:22 +0900

    adding analysis notes

---
+++ sidekick/srcs/R/Notes.R
@@ -0,0 +1,66 @@
+### Overall History and Analysis Goal
+#
+## We've made a super 'fast-onset' "NASH-driven HCC" mouse model
+#
+## Ideal model information
+#  - HCC_T : NASH(15%) + Fibrosis(15%) + Cancer(70%)
+#  - HCC_AT : NASH(47.5%) + Fibrosis(47.5%) + Cancer(5%)
+#  - CCl4 : Fibrosis(100%)
+#  - NASH : NASH(100%)
+#
+## Find cancer signal : Control vs HCC_T  ;  HCC_AT vs HCC_T  ;  Rest of all vs HCC_T ?
+## Find NASH signal : Control vs NASH  ;  CCl4 vs HCC_AT  ;  (Control + CCl4) vs HCC_AT ?
+#
+## 1. Confirm if it is certainly a 'NASH-driven HCC' onset model
+#     - find cancer signatures
+#     - show HCC groups have NASH characteristics
+#
+## 2. Compare to other existing cancer models (slow-onset models)
+#     - common characteristics
+#     - differences
+#
+## 3. If it is confirmed to be a cancer model, compare to Human HCC patients
+#     -  
+
+
+
+
+## Quality Control
+# QC plots - RLE plot
+
+
+# QC plots - Sample by Sample Heatmap
+
+
+# QC plots - PCA & MDS
+
+
+
+
+## Differential Expression Analysis - Normal vs Nash diet (No Cancer, NASH proxy)
+# 1. What are the characteristics does NASH group have?  -->  DEG & Pathway analysis
+# 2. Does NASH group represents biological signal of NASH?  -->  NASH related pathways?
+
+
+
+## Differential Expression Analysis - Normal vs CCl4 treatment (No Cancer, Fibrosis proxy)
+# 1. What are the characteristics does CCl4 group have?  -->  DEG & Pathway analysis
+# 2. Does CCl4 group represents biological signal of Hepatic Fibrosis?  -->  Fibrosis related pathways
+
+
+
+## Differential Expression Analysis - Normal vs HCC (HCC proxy)
+# 1. What are the characteristics does HCC group have?
+# 2. Does HCC group represents biological signal of Hepatocellular Carcinoma?
+
+
+
+## Differential Expression Analysis - NASH vs HCC
+# 1. What are the features does HCC group have compared to NASH group?
+
+
+
+## Differential Expression Analysis - Fibrosis vs HCC
+# 1. What are the features does HCC group have compared to CCl4 group?
+
+
